52d109f1dc01d6742b8b43a5efb8c914bf4dfefe
sjrekuc <62871510+sjrekuc@users.noreply.github.com>
2021-06-25 23:28:53 -0600

    Add files via upload
    Added code to pull target model list.

--- future/NOAA_future.py
+++ future/NOAA_future.py
@@ -14,13 +14,13 @@ with open('NOAA/USGS_NOAA.csv', 'r') as fi:
 	    USGS_gauges.append(info[0])
 	    NOAA_gauges.append(info[1].replace('\r','').replace(' ', ''))
 
-print(NOAA_gauges)
+
 # USGS_gauges = ["09067020", "09057500"]
 # NOAA_gauges = ["EALC2", "BGMC2"]
 ## start with Eagle at Avon and Blue below Green Mountain Res.
 
 
-#### write for loop that loops through each gauge starting here
+#### write for loop that loops through each NOAA gauge starting here
 for NOAA in NOAA_gauges:
 	raw_file = 'NOAA/' + NOAA + '.csv'
 	# get daily snow data from USDA
@@ -34,6 +34,7 @@ for NOAA in NOAA_gauges:
 ## iterate through each gauge file
 gauge_index = 0
 file_names = set()
+future_flow = {}
 for NOAA in NOAA_gauges:
 	raw_file = 'NOAA/' + NOAA + '.csv'
 	fi = open(raw_file, 'r')
@@ -50,7 +51,7 @@ for NOAA in NOAA_gauges:
 			date = info[0]
 			# pulls the CFS
 			flow = info[2]
-			# replaces the sl
+			# replaces the slash with a underscore in the date
 			date_filename = date.replace('/', '_') + '.csv'
 			# creates line to be written
 			new_line = USGS_gauges[gauge_index] + "=" + flow
@@ -65,11 +66,44 @@ for NOAA in NOAA_gauges:
 					fo.write(new_line)
 			# add file name to the set of file names
 			file_names.add(date_filename)
+			
+			# let's save the flow to a dictionary of dictionaries
+			# if the first key doesn't exist already, we have to create it
+			if date_filename  not in future_flow:
+			    future_flow[date_filename] = {}
+			future_flow[date_filename][USGS_gauges[gauge_index]] = float(flow.replace('\n', ''))
+			
 
 	# steps to the next gauge in line
 	gauge_index +=1
 
+# load list of target gages that we modeled
+with open('NOAA/target_models.csv', 'r') as f:
+    reader = csv.reader(f)
+    targets = list(reader)[0]
+
+print(targets)
+
+# iterate through that list of target gages
+for t in target:
+    # pull model file
+    with open('NOAA/model_' + t + '_.csv', 'r') as f:
+        reader = csv.reader(f)
+        
+
+# 
+
+# iterate through the first layer of the nested dictionary
+# 
+# loads a list of models from CSV
+# loads models from that list
+# runs the models based on those values
+# appends those gage predictions to that existing file (open file again with append)
+# DONE!
+
 # replace all of the old date files with the new ones - so that we don't have down time without this data available.
 for file in file_names:
 	shutil.copyfile(file, "public_html/NOAA/ready_" + file)
+	
+
 
